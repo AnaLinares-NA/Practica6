e8d7bcb270768c9d6a046f9bea6132028398ef30
AnaLinares-NA <ana.linares@comunidad.unam.mx>
2025-03-13 20:49:00 -0600

    Se termino con exito jijijiji

--- src/components/About.js
+++ src/components/About.js
@@ -5,21 +5,59 @@ const About = () => {
     return (
         <Typography>
             Lorem ipsum<br/>
-            Nuestro mundo en la actualidad<br/>
-            Es muy difícil y duro de pelar<br/>
-            Pero mejor hágame un gran favor<br/>
-            Y tómelo con buen humor<br/>
+            Mi esposa y yo<br/>
+            Igual que ustedes, compartimos en la vida un eterno amor<br/>
+            La dama perfecta, toda una belleza, ella es mi inspiración <br/>
+            Somos feliz ella y yo<br/>
+            Ella y yo<br/>
             <br/>
-            La hora pico<br />
-            Siéntese por favor<br />
-            La hora pico<br />
-            <br />
-            Acomódese en el sillón<br />
-            La hora pico<br />
-            Le va a gustar<br />
-            La hora pico<br />
-            Ya va a empezar<br />
-            <br />
+            Oye Don, lucha por amor<br/>
+            <br/>
+            Quizás su marido no mande en su corazón<br/>
+            <br/>
+            Oye mi pana, lucha por amor<br/>
+            <br/>
+            Quizás ese tipo no mande en su corazón<br/>
+            <br/>
+            Mi esposa y yo<br/>
+            Somos felices, dos almas, matices en lo que es el amor<br/>
+            Por eso te entiendo, y aunque sea casada, no te alejes por temor<br/>
+            No lo hagas Don, ay, no, no, no<br/>
+            <br/>
+            Ay, ya te expliqué<br/>
+            Cuando hay personas que se aman, el amor tiene que vencer<br/>
+            Y que el marido entienda que perdió a su hembra, ahora es tu mujer<br/>
+            No pueden ganar los tres<br/>
+            <br/>
+            Y te repito, lucha por amor<br/>
+            <br/>
+            Quizás su marido no mande en su corazón<br/>
+            <br/>
+            No seas tan tonto, lucha por amor<br/>
+            <br/>
+            Quizás ese tipo no mande en su corazón<br/>
+            <br/>
+            ¿Qué?<br/>
+            <br/>
+            No, yo no estoy creyendo esto<br/>
+            <br/>
+            No, no<br/>
+            <br/>
+            Que te perdone Dios, yo no lo voy a hacer<br/>
+            Los perdí a los dos, y a la misma vez<br/>
+            Ya veo que todo era mentira cuando ella me decía<br/>
+            Que se iba pa Puerto Rico a vacaciones con su amiga<br/>
+            <br/>
+            Me mintió<br/>
+            Tú y ella en una cama, allá en Bayamón<br/>
+            Quizás en Isla Verde o Carolina, ¿cuántos hoteles ensució?<br/>
+            Tú también, los odio a los dos<br/>
+            No me entiende<br/>
+            <br/>
+            Fue una traición<br/>
+            <br/>
+            Adiós...<br/>
+
         </Typography>
     );
 };
